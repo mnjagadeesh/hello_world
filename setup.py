f2a3bd6cd2e217583c59e7dcced03b38216e231b
Jagadeesh N Malakannavar <njagadeesh@zeomega.com>
2020-06-19 12:08:50 +0530

    initial checkin

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup
+
+
+setup(
+    name = "hello_world",
+    version = "0.1",
+    author = "Jagadeesh Malakannavar",
+    author_email = "mnjagadeesh@gmail.com",
+    description = ("Demo Project "),
+    license = "BSD",
+    keywords = "example documentation tutorial",
+    url = "http://packages.python.org/an_example_pypi_project",
+    packages=['hellow_world'],
+    long_description="Demo Project",
+    classifiers=[],
+
+)
